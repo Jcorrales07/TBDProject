72446d78498492cf24df810439d28e0141c99090
Joe Corrales <corralesjoe07@gmail.com>
2023-03-02 00:24:47 -0600

    Cambios en el html

--- routes/usuarios.router.js
+++ routes/usuarios.router.js
@@ -1,9 +1,7 @@
-const express = require('express')
-const pool = require('../db')
+const express = require('express');
+const pool = require('../db');
 
-const router = express.Router()
-
-
+const router = express.Router();
 
 // LOGIN
 // GET : conseguir un usuario en especifico
@@ -13,92 +11,70 @@ const router = express.Router()
 
 // Pagina de bienvenida y pues funciones generales
 
-
 // conseguir todos los usuarios
 router.get('/', async (req, res) => {
     const query = {
-        text: `SELECT * FROM usuario`
-    }
-    
+        text: `SELECT * FROM usuario`,
+    };
+
     try {
-
-
-        const allUsuarios = await pool.query(query)
-        res.json(allUsuarios.rows)
-
+        const allUsuarios = await pool.query(query);
+        res.json(allUsuarios.rows);
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
-})
+});
 
 // conseguir un usuario por id
-router.get('/:idUsuario', async (req, res) => {
-
-    const { idUsuario } = req.params
+router.get('/:username', async (req, res) => {
+    const { username } = req.params;
 
     const query = {
         text: `SELECT * FROM usuario WHERE username = '${idUsuario}'`,
-    }
+    };
 
     try {
-        
-        let dbRes = await pool.query(query)
-        res.json(dbRes.rows)
-
-    } catch (error) {
-        
-    }
-
-})
+        let dbRes = await pool.query(query);
+        res.json(dbRes.rows);
+    } catch (error) {}
+});
 
 // crear usuario
-router.post('/crear-usuario', async (req, res) => {
-
-    const { username, nombre, apellido, email, clave } = req.body
+router.post('/registrarse', async (req, res) => {
+    const { username, nombre, apellido, email, clave } = req.body;
 
     const query = {
-        text: `INSERT INTO usuario(username, nombre, apellido, email, clave)
-                    VALUES($1, $2, $3, $4)`,
-        values: [username, nombre, apellido, email, clave]
-    }
+        text: `CALL sp_create_usuario('${username}', '${nombre}', '${apellido}', '${email}', '${clave}')`,
+    };
 
-    try {   
-
+    try {
         const nuevoUsuario = await pool.query(query);
 
-        res.json(nuevoUsuario.rows)
-
+        res.json(nuevoUsuario.rows);
     } catch (error) {
-
-        console.log('Uno de los campos esta repetido compa')
-    
+        console.log('Uno de los campos esta repetido compa');
     }
-
-})
-
+});
 
 router.patch('/:username', async (req, res) => {
-    const { username } = req.params
-    const body = req.body
+    const { username } = req.params;
+    const body = req.body;
 
     res.json({
         message: 'Usuario actualizado',
         data: body,
-        username
-    })
-
-})
+        username,
+    });
+});
 
 router.delete('/:username', async (req, res) => {
-    const { username } = req.params
-    const body = req.body
+    const { username } = req.params;
+    const body = req.body;
 
     res.json({
         message: 'Usuario eliminado',
-        username
-    })
-
-})
-
+        username,
+    });
+});
 
 module.exports = router;
